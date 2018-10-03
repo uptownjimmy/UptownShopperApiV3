0d7b2bcfa84c63406a16561f9f7e7c62c1f08ca9
James McRay Johnson III <james@uptownjimmy.com>
2018-10-03 15:09:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+UptownShopperApiV3
---
+++ WebApi/Models/ItemContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models 
+{     
+    public class ItemContext : DbContext     
+    {         
+        public ItemContext(DbContextOptions<ItemContext> options) : base(options)         
+        {         
+        }       
+        public DbSet<Item> Items { get; set; }     
+  
+    } 
+}
